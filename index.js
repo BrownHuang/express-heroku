9789fc5dc6d53435081b9db398d7617fb174f079
Brown <b9602104@gmail.com>
2017-11-16 21:42:19 +0800

    commit

--- index.js
+++ index.js
@@ -3,6 +3,28 @@ const bodyParser = require('body-parser');
 const search = require('./search')
 const path = require('path')
 
+const MongoClient = require('mongodb').MongoClient;
+
+let mdb;
+// Connect to the db
+const mongoURL = 'mongodb://brown:123456@ds111496.mlab.com:11496/js-class-20171116'
+MongoClient.connect(mongoURL, function(err, db) {
+    if (err) {
+      return console.dir(err); 
+    } 
+    
+    let collection = db.collection('test');
+    let doc1 = { 'hello': 'doc1' };
+    let doc2 = { 'hello': 'doc2' };
+    let lotsOfDocs = [{ 'hello': 'doc3' }, { 'hello': 'doc4' }];
+  
+    collection.insert(doc1);
+    collection.insert(doc2, { w: 1 }, function (err, result) { });
+    collection.insert(lotsOfDocs, { w: 1 }, function (err, result) { });
+  
+    mdb = db;
+});
+
 const app = express()
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }))
